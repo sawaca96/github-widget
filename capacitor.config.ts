fb317f89bb19f914415433b7d09a19e50f25ab2c
Song <sawaca96@gmail.com>
2025-05-21 22:53:18 +0900

    Rename App

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.sawaca96.githubwidget',
-  appName: 'github-widget',
+  appName: 'GithubWidget',
   webDir: 'dist',
   plugins: {
     FirebaseAuthentication: {
